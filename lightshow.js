bc3ff2056d7a05b18fc13306805929ef4f5b4d9c
Programming <programming@us-physics-cart-18.local>
2018-04-30 12:55:18 -0400

    lightshow completed

--- lightshow.js
+++ lightshow.js
@@ -6,6 +6,13 @@ let finch = new Finch();
 
 // -----------------------------------------------
 // Don't change anything above the line
+finch.setLED(0,0,0);
+wait(5000)
+finch.setLED(255,255,255);
+wait(5000);
+finch.setLED(0,0,0);
+wait(5000);
+
 
 // Our first set of code will illuminate the Finch in one color, wait 1 second, then illuminate it in a different color
 finch.setLED(255,0,0);
@@ -13,12 +20,161 @@ wait(1000);
 finch.setLED(0,255,0);
 wait(1000);
 
+
+finch.setLED(0,0,0);
+wait(5000)
+finch.setLED(255,255,255);
+wait(5000);
+finch.setLED(0,0,0);
+wait(5000);
+finch.setLED(255,255,255);
+wait(5000);
+finch.setLED(0,0,0);
+wait(5000);
+
+
 // Traffic Light
-// Write your code here to model 3 cycles of a traffic light
+const length = 5;
+const a = length + 1;
+for (let i = 1; i < a; i += 1){
+    finch.setLED(255,0,0);
+    wait(3000);
+    finch.setLED(0,255,0);
+    wait(3000);
+    finch.setLED(255,255,0);
+    wait(3000);
+}
+
+
+finch.setLED(0,0,0);
+wait(5000)
+finch.setLED(255,255,255);
+wait(5000);
+finch.setLED(0,0,0);
+wait(5000);
+finch.setLED(255,255,255);
+wait(5000);
+finch.setLED(0,0,0);
+wait(5000);
+finch.setLED(255,255,255);
+wait(5000);
+finch.setLED(0,0,0);
+wait(5000);
 
 
 // Light show
 // Write your own code here to create a light show of your choosing
+const time = 3;
+const c = 1 + time;
+for (let b = 1; b < c; b += 1){
+    finch.setLED(255,255,255);
+    wait(3000);
+
+    finch.setLED(255,0,0);
+    wait(500);
+    finch.setLED(255,255,255);
+    wait(500);
+    finch.setLED(255,0,0);
+    wait(500);
+    finch.setLED(255,255,255);
+    wait(500);
+    finch.setLED(255,0,0);
+    wait(500);
+    finch.setLED(255,255,255);
+    wait(500);
+    finch.setLED(255,0,0);
+    wait(3000);
+
+    finch.setLED(0,0,255);
+    wait(500);
+    finch.setLED(255,0,0);
+    wait(500);
+    finch.setLED(0,0,255);
+    wait(500);
+    finch.setLED(255,0,0);
+    wait(500);
+    finch.setLED(0,0,255);
+    wait(500);
+    finch.setLED(255,0,0);
+    wait(500);
+    finch.setLED(255,0,255);
+    wait(3000);
+
+    finch.setLED(0,255,255);
+    wait(500);
+    finch.setLED(255,0,255);
+    wait(500);
+    finch.setLED(0,255,255);
+    wait(500);
+    finch.setLED(255,0,255);
+    wait(500);
+    finch.setLED(0,255,255);
+    wait(500);
+    finch.setLED(255,0,255);
+    wait(500);
+    finch.setLED(0,0,255);
+    wait(3000);
+
+    finch.setLED(0,255,0);
+    wait(500);
+    finch.setLED(0,0,255);
+    wait(500);
+    finch.setLED(0,255,0);
+    wait(500);
+    finch.setLED(0,0,255);
+    wait(500);
+    finch.setLED(0,255,0);
+    wait(500);
+    finch.setLED(0,0,255);
+    wait(500);
+    finch.setLED(0,255,255);
+    wait(3000);
+
+    finch.setLED(255,255,0);
+    wait(500);
+    finch.setLED(0,255,255);
+    wait(500);
+    finch.setLED(255,255,0);
+    wait(500);
+    finch.setLED(0,255,255);
+    wait(500);
+    finch.setLED(255,255,0);
+    wait(500);
+    finch.setLED(0,255,255);
+    wait(500);
+    finch.setLED(0,255,0);
+    wait(3000);
+
+    finch.setLED(255,0,0);
+    wait(500);
+    finch.setLED(0,255,0);
+    wait(500);
+    finch.setLED(255,0,0);
+    wait(500);
+    finch.setLED(0,255,0);
+    wait(500);
+    finch.setLED(255,0,0);
+    wait(500);
+    finch.setLED(0,255,0);
+    wait(500);
+    finch.setLED(255,255,0);
+    wait(3000);
+
+    finch.setLED(255,0,0);
+    wait(500);
+    finch.setLED(0,255,0);
+    wait(500);
+    finch.setLED(0,0,255);
+    wait(500);
+    finch.setLED(255,0,0);
+    wait(500);
+    finch.setLED(0,255,0);
+    wait(500);
+    finch.setLED(0,0,255);
+    wait(500);
+    finch.setLED(255,255,255);
+    wait(3000);
+}
 
 
 
